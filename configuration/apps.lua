b8009139d6cba9effb62fc764628a36800325311
jtb324 <james.baker@vanderbilt.edu>
2020-12-16 14:41:33 -0500

    changed the configuration so that alacritty was the terminal and thunar was the file manager also changed the theme.

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,7 +8,7 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'terminator',
+    terminal = 'alacritty',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
     quake = 'terminator',
@@ -19,7 +19,7 @@ return {
     editor = 'gedit', -- gui text editor
     social = 'discord',
     game = rofi_command,
-    files = 'nautilus',
+    files = 'thunar',
     music = rofi_command 
   },
   -- List of apps to start once on start-up
